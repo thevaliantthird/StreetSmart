7c6945c635a649a23c986c225276fc410822dd2d
thevaliantthird <shubh5796@gmail.com>
2021-05-26 18:26:29 +0530

    Merged Contributions and Organized Code

--- Infer/vd.py
+++ Infer/vd.py
@@ -1,40 +1,40 @@
-# traffic infer
 import numpy as np
+import math
 
 class Rect:
 
-	def __init__:
-		
-		self.vec = []
-		self.position = [0,0]
-		self.dim = [0,0]
-		self.traffic = 0
-
-class Roads:
-
-	def __init__:
-
-		self.length = 0
-		self.width = 0
+    def __init__(self, vec, position, dim, traffic):
+        self.vec = vec
+        self.position = position
+        self.dim = dim
+        self.traffic = traffic
 
 class Infer:
 
-	def __init__:
+    def __init__(self, RectL, IMGDIM, TrafficMap):
 
-		self.RectL = []
-		self.IMGDIM = [0,0]
-		self.TrafficMap = np.zeros(self.IMGDIM)
-
+        self.RectL = RectL
+        self.IMGDIM = IMGDIM
+        self.TrafficMap = TrafficMap
 	
-	def ConstructTraffic():
+    def ConstructTraffic(self):
+    
+	    for i in range(self.IMGDIM[0]):
+		    for j in range(self.IMGDIM[1]):
+			    for k in self.RectL:
 
-		for i in range(self.IMGDIM[0]):
-			for j in range(self.IMGDIM[0]):
-				for k in self.RectL:
+				    traffic = k.traffic
+				    xk, yk = k.position
+				    dist = math.sqrt((i-xk)**2 + (y-yk)**2)
+				    contribution = traffic/dist
 
-					traffic = k.traffic
-					xk, yk = k.position
-					dist = math.sqrt((i-xk)**2 + (y-yk)**2)
-					contribution = traffic/dist
+				    self.TrafficMap[i,j]+=contribution
 
-					self.TrafficMap[i,j]+=contribution
+
+    def ReduceNoise(self):
+        
+        for i in range(self.TrafficMap.shape[0]):
+            for j in range(self.TrafficMap.shape[1]):
+                for Rect in self.RectL:
+                    if Rect.position == (i,j):
+                        self.TrafficMap[(i-Rect.dim[0]/2):(i+Rect.dim[0]/2),(j-Rect.dim[0]/2):(j+Rect.dim[1]/2)] = 0
--- RoadID/roadparsing.py
+++ RoadID/roadparsing.py
@@ -14,7 +14,7 @@ class RoadParsing:
     def __init__(self, image):
         self.image = image
         self.bool_pxls = (image == 0).astype(int)
-    
+
     def process(self):
         label = 1
         visited = set()
@@ -30,22 +30,35 @@ class RoadParsing:
                     # print("boundary: ", boundary)
                     self.list_col.append(Collection(col,boundary,label))
                     label+=1
-                             
+
+    def RoadIter(Controller main, RoadPix road, int n):
+    i = 0
+    road.Z = 0
+    while i<n:
+        distance = ((main.BuildingList[i].Shape.x - road.coordinate.x)**2 + (main.BuildingList[i].Shape.y - road.coordinate.y)**2)**0.5
+        road.Z += distance
+        road.buildlist[i] = distance
+        i += 1
+    j = 0
+    while j<n:
+        road.buildlist[j] = float(road.buildlist[j])/road.Z
+
     
+
     def DFS(self, pixels, node, visited, col, boundary):
         if node not in visited:
             print(node)
             visited.add(node)
             col.add(node)
             if self.isboundary(node,pixels): boundary.add(node)
-            
-            if node[0] != pixels.shape[0]-1 and (pixels[node[0]+1,node[1]] == 1): 
+
+            if node[0] != pixels.shape[0]-1 and (pixels[node[0]+1,node[1]] == 1):
                 self.DFS(pixels, (node[0]+1, node[1]),visited,col,boundary)
-            if node[1] != pixels.shape[1]-1 and (pixels[node[0],node[1]+1] == 1): 
+            if node[1] != pixels.shape[1]-1 and (pixels[node[0],node[1]+1] == 1):
                 self.DFS(pixels, (node[0], node[1]+1),visited,col,boundary)
-            if node[0] != 0 and (pixels[node[0]-1,node[1]] == 1): 
-                self.DFS(pixels, (node[0]-1, node[1]),visited,col,boundary)            
-            if node[1] != 0 and (pixels[node[0],node[1]-1] == 1): 
+            if node[0] != 0 and (pixels[node[0]-1,node[1]] == 1):
+                self.DFS(pixels, (node[0]-1, node[1]),visited,col,boundary)
+            if node[1] != 0 and (pixels[node[0],node[1]-1] == 1):
                 self.DFS(pixels, (node[0], node[1]-1),visited,col,boundary)
 
     def isboundary(self,node,pixels):
@@ -53,4 +66,3 @@ class RoadParsing:
         if(pixels[node[0]+1,node[1]] == 0 or pixels[node[0],node[1]+1] == 0 or pixels[node[0]-1,node[1]]== 0 or pixels[node[0], node[1]-1] == 0): return True
         if(pixels[node[0]+1,node[1]+1] == 0  or pixels[node[0]+1,node[1]-1] == 0  or pixels[node[0]-1,node[1]+1] == 0 or pixels[node[0]-1, node[1]-1] == 0): return True
         return False
-            
